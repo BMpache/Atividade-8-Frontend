d93e6bc71f1c8e13d642881e5b3196569f78abfb
BMpache <brunopache@icloud.com>
2021-06-21 16:14:10 -0300

    sss

--- js/functions.js
+++ js/functions.js
@@ -72,11 +72,11 @@ async function callFetchWithDelete(id){
 function submitPost(){
     console.log("entrei na função");
     const form = document.forms['postForm'];    
-    const pokemon = form["pokemon"].value;
-    const type = form["type"].value;
-    const about = form["about"].value;
+    const poke = form["pokemon"].value;
+    const typ= form["type"].value;
+    const abou = form["about"].value;
     
-    callFetchWithPost(pokemon, type, about);
+    callFetchWithPost(poke, typ, abou);
     return false; // Evitar o reload da tela.
 }
 
